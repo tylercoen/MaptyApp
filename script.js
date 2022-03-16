18344ad72f7a4a6c8da25de66f12ebf8a1437b9c
Tyler Coen <43471099+tylercoen@users.noreply.github.com>
2022-03-16 19:48:28 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -10,3 +10,68 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
+let map, mapEvent; //to make it accessible in the global
+
+if (navigator.geolocation)
+  navigator.geolocation.getCurrentPosition(function (position) {
+    const { latitude } = position.coords;
+    const { longitude } = position.coords;
+    console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
+
+    const coords = [latitude, longitude];
+
+    map = L.map('map').setView(coords, 13); //13 is the zoom level, the higher the num the closer the zoom level
+
+    L.tileLayer(
+      'https://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}',
+      {
+        attribution:
+          'Tiles &copy; Esri &mdash; Source: Esri, DeLorme, NAVTEQ, USGS, Intermap, iPC, NRCAN, Esri Japan, METI, Esri China (Hong Kong), Esri (Thailand), TomTom, 2012',
+      }
+    ).addTo(map);
+    // Handling clicks on map
+    map.on(
+      //on() comes from the leaflet library
+      'click',
+      function (mapE) {
+        mapEvent = mapE;
+        form.classList.remove('hidden');
+        inputDistance.focus();
+      },
+      function () {
+        alert('Could not get your position');
+      }
+    );
+    form.addEventListener('submit', function (e) {
+      e.preventDefault();
+
+      //Clear input fields
+      inputDistance.value =
+        inputCadence.value =
+        inputDuration.value =
+        inputElevation.value =
+          '';
+      //Display marker
+      console.log(mapEvent);
+      const { lat, lng } = mapEvent.latlng;
+      L.marker([lat, lng])
+        .addTo(map)
+        .bindPopup(
+          L.popup({
+            maxWidth: 250,
+            minWidth: 100,
+            autoClose: false,
+            closeOnClick: false,
+            className: 'running-popup',
+          })
+        )
+        .setPopupContent('Workout')
+        .openPopup();
+    });
+    //All the popup options come from the leaflet documentations.
+    //Display marker
+  });
+inputType.addEventListener('change', function () {
+  inputElevation.closest('.form__row').classList.toggle('form__row--hidden');
+  inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
+});
